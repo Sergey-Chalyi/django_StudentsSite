5522e61ec7ed995ce86bb6ba7b54c3abde42cf72
Sergey_Chalyi <supersupergray@gmail.com>
2024-09-30 19:01:24 +0300

    CREATED start page

--- djangoStudentsSite/startPage/views.py
+++ djangoStudentsSite/startPage/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
 # Create your views here.
+def start(request: HttpRequest):
+    return render(request, 'startPage/index.html')
