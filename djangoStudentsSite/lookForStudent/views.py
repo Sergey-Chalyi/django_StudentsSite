eeaeec7f8098bc7d77a78036e7b2d28464e18672
Sergey_Chalyi <supersupergray@gmail.com>
2024-10-01 09:15:32 +0300

    CREATED

--- djangoStudentsSite/lookForStudent/views.py
+++ djangoStudentsSite/lookForStudent/views.py
@@ -2,4 +2,34 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 def look_student_main(request: HttpRequest):
-    return render(request, 'lookForStudent/lookForStudent_main.html')
+    data = {
+        'students': [
+            {
+                'name': 'Sergey',
+                'age': 20,
+                'specialization': 'agronomy'
+            },
+            {
+                'name': 'Anna',
+                'age': 22,
+                'specialization': 'computer science'
+            },
+            {
+                'name': 'Dmitry',
+                'age': 21,
+                'specialization': 'mathematics'
+            },
+            {
+                'name': 'Elena',
+                'age': 19,
+                'specialization': 'biology'
+            },
+            {
+                'name': 'Igor',
+                'age': 23,
+                'specialization': 'engineering'
+            }
+        ]
+    }
+
+    return render(request, 'lookForStudent/lookForStudent_main.html', data)
