8ea68de23a7bef3b31029df6b357e51323ac3860
Sergey_Chalyi <supersupergray@gmail.com>
2024-10-03 10:28:22 +0300

    ADDED model for jobs applications

--- djangoStudentsSite/lookForJob/models.py
+++ djangoStudentsSite/lookForJob/models.py
@@ -1,3 +1,31 @@
+from django.urls import reverse
+from django.utils import timezone
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Job(models.Model):
+    title = models.TextField(max_length=255)
+    on_or_off = models.TextField(max_length=2)
+    city = models.TextField(max_length=255, blank=True)
+    full_or_part = models.TextField(max_length=10)
+    salary = models.IntegerField()
+    description = models.TextField(max_length=1000, blank=True)
+
+    created_time = models.DateTimeField(auto_now_add=True)
+    changed_time = models.DateTimeField(auto_now=True)
+
+    # slug поле
+    slug = models.SlugField(max_length=255, unique=True, editable=False)
+
+    is_published = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.slug = slugify(f"{self.pk}_{self.title}")
+        super().save(update_fields=['slug'])
+
+    def get_absolute_url(self):
+        return reverse('job_blank', kwargs={'job_slug': self.slug})
+
+    def __str__(self):
+        return f"{self.pk} {self.title}"
