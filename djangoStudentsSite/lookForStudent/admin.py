e7f75ce002059ad65bc7aed34953a960b72a13f7
Sergey_Chalyi <supersupergray@gmail.com>
2024-10-06 18:49:34 +0300

    UPDATED tabs info for Student blanks in admin panel

--- djangoStudentsSite/lookForStudent/admin.py
+++ djangoStudentsSite/lookForStudent/admin.py
@@ -8,14 +8,40 @@ class StudentAdmin(admin.ModelAdmin):
     # полей в моделе добавить параметры verbose_name=''
 
     # какие поля будет видеть админ в таблице
-    list_display = ['id', 'name', 'surname', 'age', 'university', 'category', 'is_published']
+    list_display = ['id', 'name', 'surname', 'age', 'university', 'category', 'is_published', 'brief_info']
     # при нажатии на какое поле записи админу будет переходить на вкладку конкретной записи
     list_display_links = ['id', 'name']
     # сортировка
     ordering = ['id']
     # поля, которые будут доступны для редактирования еще при отображении самой таблицы
-    list_editable = ['is_published']
+    # list_editable = ['is_published']
     # сколько записей будет отображаться на одной странице
-    list_per_page = 5
+    list_per_page = 10
+
+    # для добавления пользовательского поля (его нету в бд, только в админке будет отображаться)
+    # не забываем в отображаемы е поля добавить 'brief_info'
+    # сортировки по данному полю быть не может, ибо это пользовательское поле, поэтому для сортировки
+    # это поле нужно связать с существующим
+    @admin.display(description='Description length', ordering='name')
+    def brief_info(self, student: Student):
+        return f"Description length {len(student.description)}"
+
+    # при выделении нескольких полей, с ними можно по умолчанию сделать только удаление
+    # для добавления новых действий нужно
+    actions = ['set_published', 'set_draft']
+    @admin.action(description="Publish chosen blanks")
+    def set_published(self, request, queryset):
+        # само действие
+        count = queryset.update(is_published = Student.Status.PUBLISHED)
+        # текст, который будет выводиться после применения данного действия
+        self.message_user(request, f'{count} blanks were updated')
+
+    @admin.action(description="Deactivate chosen blanks")
+    def set_draft(self, request, queryset):
+        # само действие
+        count = queryset.update(is_published=Student.Status.DRAFT)
+        # текст, который будет выводиться после применения данного действия
+        self.message_user(request, f'{count} blanks were deactivated')
+
 
 admin.site.register(SpecializationCategory)
